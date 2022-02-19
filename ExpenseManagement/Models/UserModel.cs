80a67402120cba9d946d0e4c1ffecc1211cc565b
keval gandevia <gandeviakeval05@gmail.com>
2022-02-19 16:39:58 +0530

    user services are created.

--- ExpenseManagement/Models/UserModel.cs
+++ ExpenseManagement/Models/UserModel.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace ExpenseManagement.Models
 {
+    [DataContract]
     public class UserModel
     {
+        private int intUserId;
+        private string stringUserName;
+        private string stringUserEmail;
+        private float intCredit;
+        private float intDebit;
+        private string stringPassword;
+
+        [DataMember]
+        public int UserId
+        {
+            get { return intUserId; }
+            set { intUserId = value; }
+        }
+
+        [DataMember]
+        public string UserName
+        {
+            get { return stringUserName; }
+            set { stringUserName = value; }
+        }
+
+        [DataMember]
+        public string UserEmail
+        {
+            get { return stringUserEmail; }
+            set { stringUserEmail = value; }
+        }
+
+        [DataMember]
+        public float UserCredit
+        {
+            get { return intCredit; }
+            set { intCredit = value; }
+        }
+
+        [DataMember]
+        public float UserDebit
+        {
+            get { return intDebit; }
+            set { intDebit = value; }
+        }
+
+        [DataMember]
+        public string UserPassword
+        {
+            get { return stringPassword; }
+            set { stringPassword = value; }
+        }
+
     }
 }
